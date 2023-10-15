b7174180cda6c0b940aa429fbc2e1ec63706d456
pasindusampath <pasindub32@gmail.com>
2023-10-15 12:47:49 +0530

    Error response for return Internal Error to client Initial Commit in Authenticate Micro Service

---
+++ Authenticate Micro Service/src/main/java/lk/ijse/gdse63/authenticate/dto/ErrorRes.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse63.authenticate.dto;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorRes {
+    HttpStatus httpStatus;
+    String message;
+
+    public ErrorRes(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

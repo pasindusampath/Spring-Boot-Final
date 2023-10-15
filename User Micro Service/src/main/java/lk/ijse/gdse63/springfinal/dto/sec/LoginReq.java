7189f4ecaef9b1a28822a6b6de6e83d5019484c1
pasindusampath <pasindub32@gmail.com>
2023-10-15 12:20:19 +0530

    DTO for make user login and return token Initial Commit in User Micro Service

---
+++ User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/dto/sec/LoginReq.java
@@ -0,0 +1,27 @@
+package lk.ijse.gdse63.springfinal.dto.sec;
+
+public class LoginReq {
+    private String email;
+    private String password;
+
+    public LoginReq(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

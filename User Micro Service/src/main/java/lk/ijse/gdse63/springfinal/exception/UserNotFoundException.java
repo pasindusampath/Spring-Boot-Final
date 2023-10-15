196222bb25e9bed6439d2c0d55d42b5970e2276d
pasindusampath <pasindub32@gmail.com>
2023-10-15 12:18:54 +0530

    User Not Found Custom Exception For Identify is user Available Initial Commit in User Micro Service

---
+++ User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse63.springfinal.exception;
+
+public class UserNotFoundException extends Exception{
+    public UserNotFoundException(String message){
+        super(message);
+    }
+
+}

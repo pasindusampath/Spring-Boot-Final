83b0955b365db56d611fb5f49bcc9148e88737ec
pasindusampath <pasindub32@gmail.com>
2023-10-15 12:34:11 +0530

    User Repo for manage persistence of user Initial Commit in User Micro Service

---
+++ User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse63.springfinal.repo;
+
+import lk.ijse.gdse63.springfinal.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepo extends CrudRepository<User,Integer> {
+    User findByEmail(String email);
+}

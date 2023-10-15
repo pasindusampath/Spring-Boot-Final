636454b9244557cdcd98c8524bfa61530f22bfa1
pasindusampath <pasindub32@gmail.com>
2023-10-15 16:58:47 +0530

    Custom Method for CRUD Operations of User

--- User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/service/UserService.java
+++ User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/service/UserService.java
@@ -1,9 +1,16 @@
 package lk.ijse.gdse63.springfinal.service;
 
 import lk.ijse.gdse63.springfinal.dto.UserDTO;
+import lk.ijse.gdse63.springfinal.exception.DeleteFailException;
+import lk.ijse.gdse63.springfinal.exception.UpdateFailException;
 import lk.ijse.gdse63.springfinal.exception.UserNotFoundException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService extends UserDetailsService {
     UserDTO searchUserByEmail(String email) throws UserNotFoundException;
+    void updateUser(UserDTO email) throws UpdateFailException;
+    void deleteUser(String email) throws DeleteFailException;
+    List<UserDTO> getAll(String email) throws UserNotFoundException;
 }

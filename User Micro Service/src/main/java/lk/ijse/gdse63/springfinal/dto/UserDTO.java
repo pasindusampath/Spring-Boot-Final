a8d1b3de8015c1cb61e1a52be602aa440083fcf1
pasindusampath <pasindub32@gmail.com>
2023-10-15 12:33:25 +0530

    User Entity for manage user data within user service Initial Commit in User Micro Service

---
+++ User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse63.springfinal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO {
+    private int id;
+    private String username;
+    private String password;
+    private String usernic;
+    private String contact;
+    private String email;
+    private LocalDate birthday;
+    private ArrayList<String> nicImg;
+    private String gender;
+    private String remarks;
+}

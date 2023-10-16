a936f753b5b09d4af6a5ea01c3cc4cf3db262197
pasindusampath <pasindub32@gmail.com>
2023-10-16 19:15:11 +0530

    Initial Commit Of Hotel Micro Service

---
+++ Hotel Micro Service/src/main/java/com/example/hotel_micro_service/HotelMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotel_micro_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelMicroServiceApplication.class, args);
+    }
+
+}
---
+++ Hotel Micro Service/src/test/java/com/example/hotel_micro_service/HotelMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotel_micro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

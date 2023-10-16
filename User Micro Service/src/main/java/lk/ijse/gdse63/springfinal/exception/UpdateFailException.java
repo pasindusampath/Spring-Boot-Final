8ada0f5c399389189b18bf9f121551bbab634cbc
pasindusampath <pasindub32@gmail.com>
2023-10-16 18:38:22 +0530

    Small Changes within Constructor

--- User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/exception/UpdateFailException.java
+++ User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/exception/UpdateFailException.java
@@ -5,6 +5,6 @@ public class UpdateFailException extends Exception{
         super(message);
     }
     public UpdateFailException(String message, Throwable cause) {
-        super(message, cause);
+        super(message+" :( "+ cause.getMessage(), cause);
     }
 }

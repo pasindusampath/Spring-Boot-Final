91047f495b9781e163d5b73bacce8946899e4dac
pasindusampath <pasindub32@gmail.com>
2023-10-16 18:44:01 +0530

    Added Custom Query Methods For Updating User Images

--- User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/repo/UserRepo.java
+++ User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/repo/UserRepo.java
@@ -1,9 +1,33 @@
 package lk.ijse.gdse63.springfinal.repo;
 
+import jakarta.transaction.Transactional;
 import lk.ijse.gdse63.springfinal.entity.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.sql.Date;
 
 public interface UserRepo extends CrudRepository<User,Integer> {
     User findByEmail(String email);
     void deleteByEmail(String email);
+    @Modifying
+    @Transactional
+    @Query(value = "update user u set u.profile_pic = ? , u.nic_front_img = ? , " +
+            "u.nic_rear_img = ? where u.email = ?",
+            nativeQuery = true)
+    void updateImages(String profilePic,String nicFrontImg,String nicRearImg, String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.username = :username, u.password = :password, u.usernic = :usernic, u.contact = :contact, u.birthday = :birthday, u.gender = :gender, u.remarks = :remarks WHERE u.email = :email")
+    void updateUserInfoByEmail(
+            String username,
+            String password,
+            String usernic,
+            String contact,
+            String email,
+            Date birthday,
+            String gender,
+            String remarks
+    );
 }

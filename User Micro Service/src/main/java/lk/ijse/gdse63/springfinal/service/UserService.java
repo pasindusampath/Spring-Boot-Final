484029a31989c4a476ee8287a27b6ad6e14ef7a1
pasindusampath <pasindub32@gmail.com>
2023-10-16 18:44:29 +0530

    Added Method For Register User

--- User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/service/UserService.java
+++ User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/service/UserService.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse63.springfinal.service;
 
 import lk.ijse.gdse63.springfinal.dto.UserDTO;
+import lk.ijse.gdse63.springfinal.exception.CreateFailException;
 import lk.ijse.gdse63.springfinal.exception.DeleteFailException;
 import lk.ijse.gdse63.springfinal.exception.UpdateFailException;
 import lk.ijse.gdse63.springfinal.exception.UserNotFoundException;
@@ -11,6 +12,7 @@ import java.util.List;
 public interface UserService extends UserDetailsService {
     UserDTO searchUserByEmail(String email) throws UserNotFoundException;
     void updateUser(UserDTO email) throws UpdateFailException;
+    int addUsers(UserDTO email) throws CreateFailException;
     void deleteUser(String email) throws DeleteFailException;
     List<UserDTO> getAll(String email) throws UserNotFoundException;
 }

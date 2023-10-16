0a1b393d6c57fd03f3957afe704ce0d02f9972ab
pasindusampath <pasindub32@gmail.com>
2023-10-16 22:58:24 +0530

    Moved

--- Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/HotelMicroServiceApplication.java
+++ Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/HotelMicroServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.hotel_micro_service;
+package lk.ijse.gdse63.hotel_micro_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Hotel Micro Service/src/test/java/lk/ijse/gdse63/hotel_micro_service/HotelMicroServiceApplicationTests.java
+++ Hotel Micro Service/src/test/java/lk/ijse/gdse63/hotel_micro_service/HotelMicroServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.hotel_micro_service;
+package lk.ijse.gdse63.hotel_micro_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

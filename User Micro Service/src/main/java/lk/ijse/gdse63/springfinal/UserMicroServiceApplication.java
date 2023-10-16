d1f9f21fc303cbe03c8aa89e6cbbcc880b6c17d1
pasindusampath <pasindub32@gmail.com>
2023-10-16 18:43:23 +0530

    No Changes Just Alignment

--- User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/UserMicroServiceApplication.java
+++ User Micro Service/src/main/java/lk/ijse/gdse63/springfinal/UserMicroServiceApplication.java
@@ -7,14 +7,11 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserMicroServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserMicroServiceApplication.class, args);
     }
-
-
     @Bean
-    public ModelMapper getModelMapper(){
+    public ModelMapper getModelMapper() {
         return new ModelMapper();
     }
 }

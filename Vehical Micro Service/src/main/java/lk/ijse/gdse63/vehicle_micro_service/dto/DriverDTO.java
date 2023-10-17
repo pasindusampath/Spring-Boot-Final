da745d0f953e185493a1493e7162cd4b9664be56
pasindusampath <pasindub32@gmail.com>
2023-10-17 20:01:50 +0530

    Added Getters Setters And All arg No Arg Constructor To Driver DTO With Lombok

--- Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/dto/DriverDTO.java
+++ Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/dto/DriverDTO.java
@@ -1,7 +1,14 @@
 package lk.ijse.gdse63.vehicle_micro_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DriverDTO {
     private int id;
     private String name;

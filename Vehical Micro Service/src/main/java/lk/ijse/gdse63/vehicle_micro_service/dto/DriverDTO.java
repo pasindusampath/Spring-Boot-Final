6c4740002b9ac40014ff817772922816c1ce6a7b
pasindusampath <pasindub32@gmail.com>
2023-10-17 19:58:09 +0530

    Driver DTO For Manage Driver Details within api layer and service Layer

---
+++ Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/dto/DriverDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse63.vehicle_micro_service.dto;
+
+import java.util.ArrayList;
+
+public class DriverDTO {
+    private int id;
+    private String name;
+    private String contact;
+    private String nic;
+    private String remarks;
+    private byte[] licenseImageFront;
+    private byte[] licenseImageRear;
+}

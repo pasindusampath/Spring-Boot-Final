be1d89f816c9211404bc89ba6db4ff76f364ba4d
pasindusampath <pasindub32@gmail.com>
2023-10-17 15:40:25 +0530

    Hotel DTO For Managing Hotel Data

---
+++ Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/dto/HotelDTO.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse63.hotel_micro_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HotelDTO {
+    private int id;
+    private String name;
+    private String category;
+    private boolean petAllowed;
+    private String mapLink;
+    private String address;
+    private ArrayList<String> phone;
+    private String email;
+    private ArrayList<PricesDTO> prices;
+    private String remarks;
+}

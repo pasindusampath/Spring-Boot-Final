71effc919370509f1f22ffe8e6691c085d5846a7
pasindusampath <pasindub32@gmail.com>
2023-10-17 19:59:22 +0530

    Implement Get Mapping to Catch Vehicle Data

--- Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/api/VehicleAPI.java
+++ Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/api/VehicleAPI.java
@@ -1,14 +1,28 @@
 package lk.ijse.gdse63.vehicle_micro_service.api;
 
+import lk.ijse.gdse63.vehicle_micro_service.dto.VehicleDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/vehicle")
 public class VehicleAPI {
-    @GetMapping
-    public ResponseEntity searchVehicle(){
+    @GetMapping("/{id:^0$}")
+    public ResponseEntity searchVehicle(@RequestParam String name,
+                                        @RequestParam String fuelType,
+                                        @RequestParam boolean isHybrid,
+                                        @RequestParam MultipartFile[] files,
+                                        @RequestParam double priceFor1Km,
+                                        @RequestParam double fuelUsage,
+                                        @RequestParam double priceFor100Km,
+                                        @RequestParam int noOfSeats,
+                                        @RequestParam String vehicleType,
+                                        @RequestParam String category,
+                                        @RequestParam String transmission){
+        VehicleDTO vehicleDTO = new VehicleDTO();
+
         return new ResponseEntity("Vehicle", HttpStatus.OK);
     }
 

64f0fa7a04d174f988c49b5616d21c97cdf13df3
pasindusampath <pasindub32@gmail.com>
2023-10-17 15:41:07 +0530

    Completed Request Handling within HotelApi

--- Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/api/HotelApi.java
+++ Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/api/HotelApi.java
@@ -1,27 +1,132 @@
 package lk.ijse.gdse63.hotel_micro_service.api;
 
+import lk.ijse.gdse63.hotel_micro_service.dto.HotelDTO;
+import lk.ijse.gdse63.hotel_micro_service.dto.PricesDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("api/v1/hotel")
 public class HotelApi {
-    @GetMapping
-    public void getHotel(){
+    @GetMapping("{hotelId:\\d+}")
+    public void getHotel(@PathVariable int hotelId){
+        HotelDTO hotelDTO = new HotelDTO();
+        hotelDTO.setId(hotelId);
+        hotelDTO.setName("Hotel 1");
+        hotelDTO.setCategory("Hotel");
+        hotelDTO.setPetAllowed(true);
+        hotelDTO.setMapLink("www.google.com");
+        hotelDTO.setAddress("Colombo");
+        ArrayList<String> objects = new ArrayList<>();
+        Stream.of("0771234567","0771234568").forEach(objects::add);
+        hotelDTO.setPhone(objects);
+        hotelDTO.setEmail("M5Rq7@example.com");
+        hotelDTO.setPrices(new ArrayList<>());
+        hotelDTO.setRemarks("Good");
         System.out.println("Get Mapping");
     }
 
-    @PostMapping
-    public void save(){
-        System.out.println("Post Mapping");
+    @PostMapping()
+    public ResponseEntity uploadFiles(@RequestParam("files") MultipartFile[] files,
+                                      @RequestParam("name") String name,
+                                      @RequestParam("category") String category,
+                                      @RequestParam("petAllowed") boolean petAllowed,
+                                      @RequestParam("mapLink") String mapLink,
+                                      @RequestParam("address") String address,
+                                      @RequestParam("phone") ArrayList<String> phone,
+                                      @RequestParam("email") String email,
+                                      @RequestParam("prices") ArrayList<PricesDTO> prices,
+                                      @RequestParam("remarks") String remarks) {
+
+        //Map<String,Object> result = new ObjectMapper().readValue(filtersString, HashMap.class);
+
+        System.out.println(files.length);
+        System.out.println(name);
+        System.out.println(category);
+        System.out.println(petAllowed);
+        System.out.println(mapLink);
+        System.out.println(address);
+        System.out.println(phone);
+        System.out.println(email);
+        System.out.println(prices);
+        System.out.println(remarks);
+
+        HotelDTO hotelDTO = new HotelDTO();
+        hotelDTO.setName(name);
+        hotelDTO.setCategory(category);
+        hotelDTO.setPetAllowed(petAllowed);
+        hotelDTO.setMapLink(mapLink);
+        hotelDTO.setAddress(address);
+        hotelDTO.setPhone(phone);
+        hotelDTO.setEmail(email);
+        hotelDTO.setPrices(prices);
+        hotelDTO.setRemarks(remarks);
+
+        /*Arrays.asList(files)
+                .stream()
+                .forEach(file -> fileService.uploadFile(file));
+
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded all files!");
+
+        return "redirect:/";*/
+        return new ResponseEntity(1, HttpStatus.CREATED);
     }
 
-    @PutMapping
-    public void update(){
-        System.out.println("Put Mapping");
+    @PutMapping("/{id:\\d+}")
+    public ResponseEntity update(@PathVariable String id,
+                       @RequestParam("files") MultipartFile[] files,
+                       @RequestParam("name") String name,
+                       @RequestParam("category") String category,
+                       @RequestParam("petAllowed") boolean petAllowed,
+                       @RequestParam("mapLink") String mapLink,
+                       @RequestParam("address") String address,
+                       @RequestParam("phone") ArrayList<String> phone,
+                       @RequestParam("email") String email,
+                       @RequestParam("prices") ArrayList<PricesDTO> prices,
+                       @RequestParam("remarks") String remarks){
+
+        System.out.println(files.length);
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println(category);
+        System.out.println(petAllowed);
+        System.out.println(mapLink);
+        System.out.println(address);
+        System.out.println(phone);
+        System.out.println(email);
+        System.out.println(prices);
+        System.out.println(remarks);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @DeleteMapping
-    public void delete(){
-        System.out.println("Delete Mapping");
+    @DeleteMapping("/{id:\\d+}")
+    public ResponseEntity delete(@PathVariable String id){
+        System.out.println(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @GetMapping("/{starRate:^Star-[2-5]$}")
+    public ResponseEntity getByStarRate(@PathVariable String starRate){
+        int star = Integer.parseInt((starRate.split("-"))[1]);
+        System.out.println(star);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity getAll(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }

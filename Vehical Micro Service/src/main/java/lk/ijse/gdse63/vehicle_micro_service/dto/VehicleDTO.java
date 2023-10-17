9b6870edba155d5ca18696fc8fbf61e06cac6fc2
pasindusampath <pasindub32@gmail.com>
2023-10-17 19:58:34 +0530

    Vehicle DTO For Manage Vehicle Details within api layer and service Layer

---
+++ Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/dto/VehicleDTO.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse63.vehicle_micro_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VehicleDTO {
+    private int id;
+    private String name;
+    private String fuelType;
+    private boolean isHybrid;
+    private ArrayList<byte[]> images;
+    private double priceFor1Km;
+    private double fuelUsage;
+    private double priceFor100Km;
+    private int seatCapacity;
+    private String vehicleType;
+    private String category;
+}

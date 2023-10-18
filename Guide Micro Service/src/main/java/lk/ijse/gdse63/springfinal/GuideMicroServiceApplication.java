8118b8d66eb46f21215774b443d0a38f3e0d1c10
pasindusampath <pasindub32@gmail.com>
2023-10-18 06:44:43 +0530

    Initial Commit Oh Guide Micro Service

---
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/GuideMicroServiceApplication.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse63.springfinal;
+
+import org.springframework.boot.SpringApplication;
+
+public class GuideMicroServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GuideMicroServiceApplication.class, args);
+    }
+
+}
---
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/ServletInitializer.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse63.springfinal;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GuideMicroServiceApplication.class);
+    }
+
+}

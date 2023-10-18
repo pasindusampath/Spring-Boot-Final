a434eb2907cf352257704d6e23d10fcba90f5ab7
pasindusampath <pasindub32@gmail.com>
2023-10-18 21:59:41 +0530

    Added Guide DTO to Manage Guide Details

---
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/GuideDTO.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse63.springfinal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GuideDTO {
+    private int id;
+    private String name;
+    private String address;
+    private String contact;
+    private LocalDate birthDate;
+    private double manDayValue;
+    private String experience;
+    private byte[] guideIdFront;
+    private byte[] guideIdRear;
+    private byte[] NicFront;
+    private byte[] NicRear;
+    private byte[] profilePic;
+}

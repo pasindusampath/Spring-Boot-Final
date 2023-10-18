3473e548d78cc769566fdcdc158af2d5e6839b2d
pasindusampath <pasindub32@gmail.com>
2023-10-18 22:14:32 +0530

    Added Parameters to catch Data on Post Request

--- Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/api/GuideAPI.java
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/api/GuideAPI.java
@@ -2,12 +2,24 @@ package lk.ijse.gdse63.springfinal.api;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping("/api/v1/guide")
 public class GuideAPI {
 
     @PostMapping
-    public void saveGuide() {
+    public void saveGuide(@RequestParam("name")String name,
+                          @RequestParam("address")String address,
+                          @RequestParam("contact") String contact,
+                          @RequestParam("birthDate") LocalDate birthDate,
+                          @RequestParam("manDayValue") double manDayValue,
+                          @RequestParam("experience") String experience,
+                          @RequestParam("guideIdFront") byte[] guideIdFront,
+                          @RequestParam("guideIdRear") byte[] guideIdRear,
+                          @RequestParam("nicFront") byte[] nicFront,
+                          @RequestParam("nicRear") byte[] nicRear,
+                          @RequestParam("profilePic") byte[] profilePic) {
 
     }
 

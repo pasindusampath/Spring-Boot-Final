a4548237cc733fd5178b0fb003bbdcdba2043133
pasindusampath <pasindub32@gmail.com>
2023-10-18 23:45:18 +0530

    Added Missing Annotation

--- Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/GuideMicroServiceApplication.java
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/GuideMicroServiceApplication.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse63.springfinal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class GuideMicroServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GuideMicroServiceApplication.class, args);

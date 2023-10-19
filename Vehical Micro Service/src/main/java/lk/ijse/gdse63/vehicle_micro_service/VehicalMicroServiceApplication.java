94dc50ec761ddbc95c47e21762d630e6fe2eaef5
pasindusampath <pasindub32@gmail.com>
2023-10-19 19:47:11 +0530

    Added Model Mapper And Gson to Spring Container

--- Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/VehicalMicroServiceApplication.java
+++ Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/VehicalMicroServiceApplication.java
@@ -1,13 +1,26 @@
 package lk.ijse.gdse63.vehicle_micro_service;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VehicalMicroServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VehicalMicroServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public Gson getGson(){
+        return new Gson();
     }
 
 }

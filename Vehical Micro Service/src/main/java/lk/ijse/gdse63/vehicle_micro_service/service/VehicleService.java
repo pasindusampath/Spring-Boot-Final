c890cfe13e34c9888dd1a4bf445b24086b73c02c
pasindusampath <pasindub32@gmail.com>
2023-10-19 20:43:04 +0530

    Added Search Abstract Method To Vehicle Service

--- Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/service/VehicleService.java
+++ Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/service/VehicleService.java
@@ -2,8 +2,11 @@ package lk.ijse.gdse63.vehicle_micro_service.service;
 
 import lk.ijse.gdse63.vehicle_micro_service.dto.DriverDTO;
 import lk.ijse.gdse63.vehicle_micro_service.dto.VehicleDTO;
+import lk.ijse.gdse63.vehicle_micro_service.exception.NotFoundException;
 import lk.ijse.gdse63.vehicle_micro_service.exception.SaveFailException;
 
 public interface VehicleService {
     int saveVehicle(VehicleDTO dto) throws SaveFailException;
+
+    VehicleDTO searchVehicle(int id) throws NotFoundException;
 }

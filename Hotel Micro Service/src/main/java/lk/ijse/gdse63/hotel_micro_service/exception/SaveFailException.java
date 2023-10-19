8a4e906e990e91a419889bfccbd85566a9490f8d
pasindusampath <pasindub32@gmail.com>
2023-10-19 23:56:00 +0530

    Custom Exceptions to identify Save and Update failure

---
+++ Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/exception/SaveFailException.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse63.hotel_micro_service.exception;
+
+public class SaveFailException extends Exception{
+    public SaveFailException(String message){
+        super(message);
+    }
+
+    public SaveFailException(String message,Throwable cause){
+        super(message,cause);
+    }
+}

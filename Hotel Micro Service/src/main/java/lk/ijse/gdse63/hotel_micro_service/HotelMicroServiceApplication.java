bad2b1dd15ffb25c248e5b22bbdeecc64e7bb218
pasindusampath <pasindub32@gmail.com>
2023-10-19 23:54:47 +0530

    Added Gson and Model Mapper as beans

--- Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/HotelMicroServiceApplication.java
+++ Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/HotelMicroServiceApplication.java
@@ -1,7 +1,10 @@
 package lk.ijse.gdse63.hotel_micro_service;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HotelMicroServiceApplication {
@@ -10,4 +13,12 @@ public class HotelMicroServiceApplication {
         SpringApplication.run(HotelMicroServiceApplication.class, args);
     }
 
+    @Bean
+    public Gson getGson(){
+        return new Gson();
+    }
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
 }

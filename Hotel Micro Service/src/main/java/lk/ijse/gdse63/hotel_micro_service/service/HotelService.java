0f1bf55041baf1fa0399fb2e4986180fab185cc7
pasindusampath <pasindub32@gmail.com>
2023-10-20 00:14:48 +0530

    Added 2 more methods to handle basic crud Operation

--- Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/service/HotelService.java
+++ Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/service/HotelService.java
@@ -1,10 +1,14 @@
 package lk.ijse.gdse63.hotel_micro_service.service;
 
 import lk.ijse.gdse63.hotel_micro_service.dto.HotelDTO;
+import lk.ijse.gdse63.hotel_micro_service.exception.DeleteFailException;
+import lk.ijse.gdse63.hotel_micro_service.exception.NotFoundException;
 import lk.ijse.gdse63.hotel_micro_service.exception.SaveFailException;
 import lk.ijse.gdse63.hotel_micro_service.exception.UpdateFailException;
 
 public interface HotelService {
     int save(HotelDTO hotelDTO) throws SaveFailException;
     void update(HotelDTO hotelDTO) throws UpdateFailException;
+    void delete(int id) throws DeleteFailException, NotFoundException;
+    HotelDTO search(int id) throws NotFoundException;
 }

33e1fabd8de9ed9cc2b57cf7b58c235ecdb3c9d6
pasindusampath <pasindub32@gmail.com>
2023-10-19 19:44:39 +0530

    Driver Repo to Manage Persistence Of Driver Data

---
+++ Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/repo/DriverRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse63.vehicle_micro_service.repo;
+
+import lk.ijse.gdse63.vehicle_micro_service.entity.Driver;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DriverRepo extends CrudRepository<Driver,Integer> {
+}

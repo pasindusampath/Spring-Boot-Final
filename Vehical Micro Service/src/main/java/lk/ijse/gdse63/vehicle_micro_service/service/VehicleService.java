be9a55981bf4d955c00d1b6f94ce10a609e2b0d3
pasindusampath <pasindub32@gmail.com>
2023-10-19 21:24:31 +0530

    Added Abstract Method To Find Vehicles By Category

--- Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/service/VehicleService.java
+++ Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/service/VehicleService.java
@@ -5,8 +5,13 @@ import lk.ijse.gdse63.vehicle_micro_service.dto.VehicleDTO;
 import lk.ijse.gdse63.vehicle_micro_service.exception.NotFoundException;
 import lk.ijse.gdse63.vehicle_micro_service.exception.SaveFailException;
 
+import java.util.List;
+
 public interface VehicleService {
     int saveVehicle(VehicleDTO dto) throws SaveFailException;
 
     VehicleDTO searchVehicle(int id) throws NotFoundException;
+
+    List<VehicleDTO> searchByCategory(String category) throws NotFoundException;
+
 }

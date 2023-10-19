0b893bb012b70c8c5ca3ea6b8ee6b2f5cdea5a1d
pasindusampath <pasindub32@gmail.com>
2023-10-19 23:55:15 +0530

    Hotel Service to Manage Business logics of hotels

--- Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/service/HotelService.java
+++ Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/service/HotelService.java
@@ -1,4 +1,10 @@
 package lk.ijse.gdse63.hotel_micro_service.service;
 
+import lk.ijse.gdse63.hotel_micro_service.dto.HotelDTO;
+import lk.ijse.gdse63.hotel_micro_service.exception.SaveFailException;
+import lk.ijse.gdse63.hotel_micro_service.exception.UpdateFailException;
+
 public interface HotelService {
+    int save(HotelDTO hotelDTO) throws SaveFailException;
+    void update(HotelDTO hotelDTO) throws UpdateFailException;
 }

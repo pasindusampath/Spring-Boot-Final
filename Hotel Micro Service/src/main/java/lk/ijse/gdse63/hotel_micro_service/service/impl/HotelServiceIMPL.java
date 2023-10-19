4a177f9be633a94ca39bd2b2fbe3bd81e75fe293
pasindusampath <pasindub32@gmail.com>
2023-10-19 23:32:51 +0530

    Hotel Service Implementation Initial Commit

---
+++ Hotel Micro Service/src/main/java/lk/ijse/gdse63/hotel_micro_service/service/impl/HotelServiceIMPL.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse63.hotel_micro_service.service.impl;
+
+import lk.ijse.gdse63.hotel_micro_service.service.HotelService;
+
+public class HotelServiceIMPL implements HotelService {
+}

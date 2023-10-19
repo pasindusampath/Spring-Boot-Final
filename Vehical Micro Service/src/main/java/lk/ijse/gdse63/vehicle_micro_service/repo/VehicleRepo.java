80b7b759e46537277d80f574cafb331fb847f730
pasindusampath <pasindub32@gmail.com>
2023-10-19 21:24:20 +0530

    Added Method To Find Vehicles By Category

--- Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/repo/VehicleRepo.java
+++ Vehical Micro Service/src/main/java/lk/ijse/gdse63/vehicle_micro_service/repo/VehicleRepo.java
@@ -3,5 +3,8 @@ package lk.ijse.gdse63.vehicle_micro_service.repo;
 import lk.ijse.gdse63.vehicle_micro_service.entity.Vehicle;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface VehicleRepo extends CrudRepository<Vehicle,Integer> {
+    List<Vehicle> findByCategory(String category);
 }

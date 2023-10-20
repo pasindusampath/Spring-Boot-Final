b3717546cdf4bdce44380abc80be619e41a0a54a
pasindusampath <pasindub32@gmail.com>
2023-10-20 19:03:33 +0530

    Guide Entity To Persist Data of Guide

---
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/entity/Guide.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse63.springfinal.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+@Entity
+public class Guide {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String address;
+    private String contact;
+    private Date birthDate;
+    private double manDayValue;
+    private String experience;
+    private String guideIdFront;
+    private String guideIdRear;
+    private String nicFront;
+    private String nicRear;
+    private String profilePic;
+}

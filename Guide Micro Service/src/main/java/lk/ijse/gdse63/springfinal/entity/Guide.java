cb4d97f0dd95eb4e057820912cf7d2a5a35e8e63
pasindusampath <pasindub32@gmail.com>
2023-10-20 19:15:37 +0530

    Entity Changes Added Getters Setters and constructors using lombok

--- Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/entity/Guide.java
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/entity/Guide.java
@@ -4,9 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Guide {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

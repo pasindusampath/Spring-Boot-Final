7fc989a16074b6a5b2cae60bb39e8c1a5f13075e
pasindusampath <pasindub32@gmail.com>
2023-10-20 19:03:12 +0530

    Guide Repo To Manage Persistence Of Guide

---
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/repo/GuideRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse63.springfinal.repo;
+
+import lk.ijse.gdse63.springfinal.entity.Guide;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GuideRepo extends CrudRepository<Guide, Integer> {
+}

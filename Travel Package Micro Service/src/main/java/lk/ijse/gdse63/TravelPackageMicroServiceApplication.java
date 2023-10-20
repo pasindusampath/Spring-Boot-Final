1c08f8a6f9fc8d27fda4dfd611501ba23d913de6
pasindusampath <pasindub32@gmail.com>
2023-10-20 23:17:01 +0530

    Initial Commit Travel Package

---
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/TravelPackageMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse63;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPackageMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelPackageMicroServiceApplication.class, args);
+    }
+
+}

163077fb04bf3e2492bd7b8059c83d27bc1a2fc2
pasindusampath <pasindub32@gmail.com>
2023-10-20 19:22:23 +0530

    Added Simple Operation Methods

--- Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/service/GuidService.java
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/service/GuidService.java
@@ -1,5 +1,10 @@
 package lk.ijse.gdse63.springfinal.service;
 
-public interface GuidService {
+import lk.ijse.gdse63.springfinal.dto.GuideDTO;
 
+public interface GuidService {
+    int saveGuide(GuideDTO guideDTO);
+    void updateGuide(GuideDTO guideDTO);
+    void deleteGuide(int id);
+    GuideDTO getGuide(int id);
 }

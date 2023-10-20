b817faca782717fc7334b802e2c690b3775d0ba8
pasindusampath <pasindub32@gmail.com>
2023-10-20 20:36:37 +0530

    Added throw clause

--- Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/service/GuidService.java
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/service/GuidService.java
@@ -1,12 +1,13 @@
 package lk.ijse.gdse63.springfinal.service;
 
 import lk.ijse.gdse63.springfinal.dto.GuideDTO;
+import lk.ijse.gdse63.springfinal.exception.DeleteFailException;
 import lk.ijse.gdse63.springfinal.exception.SaveFailException;
 import lk.ijse.gdse63.springfinal.exception.UpdateFailException;
 
 public interface GuidService {
     int saveGuide(GuideDTO guideDTO) throws SaveFailException;
     void updateGuide(GuideDTO guideDTO) throws UpdateFailException;
-    void deleteGuide(int id);
+    void deleteGuide(int id) throws DeleteFailException;
     GuideDTO getGuide(int id);
 }

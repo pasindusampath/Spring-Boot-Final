9c87864642bbdd11f7fdc52cc2219404f347e9af
pasindusampath <pasindub32@gmail.com>
2023-10-20 20:08:22 +0530

    Added Model Mapper and Gson to Spring context

--- Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/GuideMicroServiceApplication.java
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/springfinal/GuideMicroServiceApplication.java
@@ -1,12 +1,24 @@
 package lk.ijse.gdse63.springfinal;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GuideMicroServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GuideMicroServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+    @Bean
+    public Gson getGson(){
+        return new Gson();
     }
 
 }

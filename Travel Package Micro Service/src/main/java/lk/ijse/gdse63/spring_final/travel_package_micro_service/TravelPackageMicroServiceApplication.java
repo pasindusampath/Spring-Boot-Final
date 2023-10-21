fef68c2f55f7702626d9f955c28544cf9b64b626
pasindusampath <pasindub32@gmail.com>
2023-10-21 14:57:55 +0530

    Added Model Mapper to Spring context

--- Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/TravelPackageMicroServiceApplication.java
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/TravelPackageMicroServiceApplication.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse63.spring_final.travel_package_micro_service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TravelPackageMicroServiceApplication {
@@ -10,4 +12,8 @@ public class TravelPackageMicroServiceApplication {
         SpringApplication.run(TravelPackageMicroServiceApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
 }

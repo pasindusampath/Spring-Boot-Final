7617b9d15fca91ce15fa6a41d42fdb682c4de79e
pasindusampath <pasindub32@gmail.com>
2023-10-21 12:01:31 +0530

    Travel Package Service with basic operations Initial Commit

--- Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/service/TravelPackageService.java
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/service/TravelPackageService.java
@@ -1,4 +1,16 @@
 package lk.ijse.gdse63.spring_final.travel_package_micro_service.service;
 
-public class TravelPackageService {
+import lk.ijse.gdse63.spring_final.travel_package_micro_service.dto.TravelPackageDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface TravelPackageService {
+
+    public String save(TravelPackageDTO obj);
+    void update(TravelPackageDTO obj);
+    void delete(String id);
+    List<TravelPackageDTO> getPackagesByCategory(String category);
+
 }

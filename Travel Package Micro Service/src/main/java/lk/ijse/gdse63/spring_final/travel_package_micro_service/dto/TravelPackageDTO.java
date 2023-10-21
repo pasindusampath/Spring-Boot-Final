fc613924b295e92c59142d5255df2d70fcc3448a
pasindusampath <pasindub32@gmail.com>
2023-10-21 11:46:53 +0530

    Added travel Package DTO to manage Travel Package Details

---
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/dto/TravelPackageDTO.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse63.spring_final.travel_package_micro_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TravelPackageDTO {
+    private String id;
+    private int hotelCount;
+    private int areaCount;
+    private double estimatedPrice;
+    private String category;
+}

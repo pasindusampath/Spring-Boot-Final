1139c12bda32c18c2b6bbb5d0bf576136ff9f17b
pasindusampath <pasindub32@gmail.com>
2023-10-21 12:00:31 +0530

    Travel Package Service Injection to Travel Package API

--- Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/api/TravelPackageApi.java
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/api/TravelPackageApi.java
@@ -1,15 +1,19 @@
 package lk.ijse.gdse63.spring_final.travel_package_micro_service.api;
 
 import lk.ijse.gdse63.spring_final.travel_package_micro_service.dto.TravelPackageDTO;
+import lk.ijse.gdse63.spring_final.travel_package_micro_service.service.TravelPackageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/travel-package")
 public class TravelPackageApi {
+    TravelPackageService service;
+    public TravelPackageApi(TravelPackageService service){
+        this.service = service;
+    }
     @PostMapping
     public ResponseEntity save(@RequestBody TravelPackageDTO obj){
-
         return ResponseEntity.ok().build();
     }
 

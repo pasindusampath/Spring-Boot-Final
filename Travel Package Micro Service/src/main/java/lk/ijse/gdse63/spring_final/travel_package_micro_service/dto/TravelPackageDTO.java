bdd700215f7d7c1641bad93add3d3ed7be05a4aa
pasindusampath <pasindub32@gmail.com>
2023-10-21 14:57:38 +0530

    Minor Changes

--- Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/dto/TravelPackageDTO.java
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/dto/TravelPackageDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TravelPackageDTO {
+public class TravelPackageDTO{
     private String id;
     private int hotelCount;
     private int areaCount;

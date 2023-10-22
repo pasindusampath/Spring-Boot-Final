56cc8a7c1bf63d80846c14218a8326d753f28ae0
pasindusampath <pasindub32@gmail.com>
2023-10-22 12:41:45 +0530

    Find All Method Created

--- Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/repo/TravelPackageRepo.java
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/repo/TravelPackageRepo.java
@@ -1,10 +1,15 @@
 package lk.ijse.gdse63.spring_final.travel_package_micro_service.repo;
 
 import lk.ijse.gdse63.spring_final.travel_package_micro_service.entity.TravelPackage;
+import org.springframework.data.mongodb.repository.Aggregation;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TravelPackageRepo extends CrudRepository<TravelPackage, String> {
     List<TravelPackage> findByCategory(String category);
+    List<TravelPackage> findAll();
+
 }

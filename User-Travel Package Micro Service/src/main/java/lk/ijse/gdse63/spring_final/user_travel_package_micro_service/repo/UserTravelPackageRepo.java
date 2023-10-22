1b9eff5c5a51288e7b4bade1e2e185aced7f63eb
pasindusampath <pasindub32@gmail.com>
2023-10-22 21:40:07 +0530

    User Travel Package Repo For Manage Persistence

---
+++ User-Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/user_travel_package_micro_service/repo/UserTravelPackageRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse63.spring_final.user_travel_package_micro_service.repo;
+
+import lk.ijse.gdse63.spring_final.user_travel_package_micro_service.entity.UserTravelPackage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserTravelPackageRepo extends CrudRepository<UserTravelPackage,Integer> {
+}

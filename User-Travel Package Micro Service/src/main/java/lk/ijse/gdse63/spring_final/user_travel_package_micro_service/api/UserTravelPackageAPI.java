96ff5e495bad603b404ade956b30c2684f26d42e
pasindusampath <pasindub32@gmail.com>
2023-10-22 20:31:05 +0530

    Api For Manage Endpoints

---
+++ User-Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/user_travel_package_micro_service/api/UserTravelPackageAPI.java
@@ -0,0 +1,70 @@
+package lk.ijse.gdse63.spring_final.user_travel_package_micro_service.api;
+
+
+import lk.ijse.gdse63.spring_final.user_travel_package_micro_service.dto.UserTravelPackageDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user-travel-package")
+public class UserTravelPackageAPI {
+
+    @GetMapping()
+    public ResponseEntity get(){
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity post(@RequestBody UserTravelPackageDTO obj){
+        System.out.println(obj);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity savePaymentData(@RequestParam("value") double price,
+                                          @RequestPart("paymentSlip") byte[] paymentSlip){
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity put(){
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity delete(){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/paymentOfPackage/{id:\\d+}")
+    public ResponseEntity getPaymentsOfPackage(@PathVariable int id){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/unPayedPackagesOfUser/{userId:\\d+}")
+    public ResponseEntity getUnPayedPackagesOfUser(@PathVariable int userId){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/historyOfUser/{userId:\\d+}")
+    public ResponseEntity historyOfUser(@PathVariable int userId){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/needApprovalPayments")
+    public ResponseEntity getNeedApprovalPayments(){
+        return ResponseEntity.ok().build();
+    }
+    @PostMapping("/approvePayment/{paymentId:\\d+}")
+    public ResponseEntity approvePayment(@PathVariable int paymentId){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/monthlyIncome")
+    public ResponseEntity getMonthlyIncome(){
+        return ResponseEntity.ok("Income");
+    }
+
+
+}

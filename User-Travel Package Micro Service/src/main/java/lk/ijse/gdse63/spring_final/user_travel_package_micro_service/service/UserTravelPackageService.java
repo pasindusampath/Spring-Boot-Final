27a57dc5d284e10e1593042eb26d6e95a34a3651
pasindusampath <pasindub32@gmail.com>
2023-10-22 23:44:36 +0530

    Added Save Operation To Service Layer

--- User-Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/user_travel_package_micro_service/service/UserTravelPackageService.java
+++ User-Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/user_travel_package_micro_service/service/UserTravelPackageService.java
@@ -1,8 +1,11 @@
 package lk.ijse.gdse63.spring_final.user_travel_package_micro_service.service;
 
+import lk.ijse.gdse63.spring_final.user_travel_package_micro_service.dto.UserTravelPackageDTO;
+import lk.ijse.gdse63.spring_final.user_travel_package_micro_service.exception.SaveFailException;
 import org.springframework.stereotype.Service;
 
 
 public interface UserTravelPackageService {
+    int save(UserTravelPackageDTO ob) throws SaveFailException;
 
 }

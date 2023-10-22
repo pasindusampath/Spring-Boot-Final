b254893e7f53e213cdf12d2e412f9a10f4570f6c
pasindusampath <pasindub32@gmail.com>
2023-10-22 12:40:58 +0530

    Added Generate Next Id Feature

--- Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/service/impl/TravelPackageServiceIMPL.java
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/travel_package_micro_service/service/impl/TravelPackageServiceIMPL.java
@@ -11,17 +11,26 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.TreeSet;
 
 @Service
 public class TravelPackageServiceIMPL implements TravelPackageService {
-    TravelPackageRepo travelPackageRepo ;
+    TravelPackageRepo travelPackageRepo;
     ModelMapper modelMapper;
-    public TravelPackageServiceIMPL(TravelPackageRepo travelPackageRepo, ModelMapper modelMapper) {
+
+
+
+    public TravelPackageServiceIMPL(TravelPackageRepo travelPackageRepo,
+                                    ModelMapper modelMapper) {
         this.travelPackageRepo = travelPackageRepo;
-        this.modelMapper=modelMapper;
+        this.modelMapper = modelMapper;
+
     }
+
     @Override
     public String save(TravelPackageDTO obj) {
+        String id = generateNextId();
+        obj.setId(id);
         TravelPackage map = modelMapper.map(obj, TravelPackage.class);
         TravelPackage save = travelPackageRepo.save(map);
         return save.getId();
@@ -46,7 +55,7 @@ public class TravelPackageServiceIMPL implements TravelPackageService {
     @Override
     public TravelPackageDTO fidById(String id) {
         Optional<TravelPackage> byId = travelPackageRepo.findById(id);
-        if (byId.isPresent()){
+        if (byId.isPresent()) {
             return modelMapper.map(byId.get(), TravelPackageDTO.class);
         }
         return null;
@@ -55,7 +64,22 @@ public class TravelPackageServiceIMPL implements TravelPackageService {
     @Override
     public List<TravelPackageDTO> findByCategory(String category) {
         List<TravelPackage> byCategory = travelPackageRepo.findByCategory(category);
-        ArrayList<TravelPackageDTO> list = modelMapper.map(byCategory,new TypeToken<ArrayList<TravelPackageDTO>>(){}.getType());
+        ArrayList<TravelPackageDTO> list = modelMapper.map(byCategory, new TypeToken<ArrayList<TravelPackageDTO>>() {
+        }.getType());
         return list;
+    }
+
+    @Override
+    public String generateNextId() {
+        ArrayList<String> ids = new ArrayList<>();
+        TreeSet<Integer> numarical = new TreeSet<>();
+        List<TravelPackage> all = travelPackageRepo.findAll();
+        all.stream().map(TravelPackage::getId).forEach(ids::add);
+        ids.forEach(e->{
+            numarical.add(Integer.parseInt(e.split("NEXT-")[1]));
+        });
+
+        return String.format("NEXT-%05d", numarical.last() + 1);
+
     }
 }

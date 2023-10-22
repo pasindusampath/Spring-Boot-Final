c7374f87accfe48b0d3c2a7d29e9009e3c6bcd4c
pasindusampath <pasindub32@gmail.com>
2023-10-22 21:32:53 +0530

    User Travel Package Service Initial Commit

---
+++ User-Travel Package Micro Service/src/main/java/lk/ijse/gdse63/spring_final/user_travel_package_micro_service/service/UserTravelPackageService.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse63.spring_final.user_travel_package_micro_service.service;
+
+import org.springframework.stereotype.Service;
+
+
+public interface UserTravelPackageService {
+
+}
